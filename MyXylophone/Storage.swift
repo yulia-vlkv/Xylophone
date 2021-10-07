50195d12a93e1602f18689abcdef344fe4e66620
Iuliia Vlkv <yu.wlkw@gmail.com>
2021-10-07 22:08:51 +0500

    Minor fixes

--- MyXylophone/Storage.swift
+++ MyXylophone/Storage.swift
@@ -20,22 +20,3 @@ var letters = ["C", "D", "E", "F", "G", "A", "B"]
 
 var insets: Array<CGFloat> = [70, 80, 90, 100, 110, 120, 130]
 
-
-
-//class customButton: UIButton {
-//    var index: Int
-//    var color: UIColor
-//    var inset: CGFloat
-//    var letter: String
-//    
-//    init(index: Int, color: UIColor, inset: CGFloat, letter: String) {
-//        self.index = index
-//        self.color = color
-//        self.inset = inset
-//        self.letter = letter
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
